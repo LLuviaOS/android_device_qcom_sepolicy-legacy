PixelExperience/device_qcom_sepolicy-legacy-um

sepolicy: Include wifi-ext sepol
Change-Id: Idf06388880d0b79a0fadb290129ec97999e19879

sepolicy.mk
@@ -28,3 +28,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
 -include device/custom/sepolicy/qcom/sepolicy.mk
+-include device/custom/sepolicy/wifi-ext/sepolicy.mk

LLuviaOS/android_device_qcom_sepolicy-legacy

Signed-off-by: Ram9199 <sriramscience7@gmail.com>

sepolicy.mk
@@ -28,3 +28,4 @@ BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
     device/qcom/sepolicy-legacy-um/private
 
 -include device/lluvia/sepolicy/qcom/sepolicy.mk
+-include device/lluvia/sepolicy/wifi-ext/sepolicy.mk